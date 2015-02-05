8eb5de1b5b592acdb7eccce3815b094ec96f37ae
Chris Bidler <cbidler@spscommerce.com>
2015-02-05 11:52:18 -0500

    Tests pass. Crude, but effective. Will blow stack.

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -7,5 +7,50 @@
                (slurp)
                (read-string)))
 
-(defn doublets [word1 word2]
-  "make me work")
+(defn shared-letters
+  "Given two strings of equal length, returns a bitfield showing letters which match
+  (shared-letters \"abcd\" \"abce\")
+  => [1 1 1 0]"
+  [one two]
+  (if (= (.length one) (.length two))
+    (vec (map #(if (= %1 %2) 1 0)
+              (.toCharArray one)
+              (.toCharArray two)))
+    []))
+
+(defn neighbor?
+  [letters]
+  (= 1 (count (filter zero? letters))))
+
+(defn find-neighbors
+  "Given a word and a list of words, find the 'neighbors' of the word in the list"
+  [word list]
+  (let [search word
+        my-map (zipmap (map #(shared-letters search %) list) list)
+        my-keys (filter neighbor? (keys my-map))]
+    (into [] (map #(get my-map %) my-keys))))
+
+(defn new-neighbors
+  "Filter from neighbors of word in word-list elements we've seen"
+  [word word-list seen]
+  (filter #(not (some #{%} seen)) (find-neighbors word word-list)))
+
+(defn check-neighbors
+  "Walk the graph of new-neighbors until we find target or find we can't go further"
+   [target path list seen]
+   (if (empty? (filter #(not (some #{%} seen)) list))
+       []
+       (if (some #{target} list)
+           (conj path target)
+           (map #(check-neighbors
+             target
+             (conj path %)
+             (new-neighbors % words seen)
+             (conj seen %)) list))))
+
+(defn doublets
+  "Blow stack and die in a beautiful pyre, or maybe just return the path from word1 to word2"
+  [word1 word2]
+  (if (= (.length word1) (.length word2))
+      (flatten (check-neighbors word2 [word1] (find-neighbors word1 words) #{word1}))
+      []))
--- doublets/test/doublets/solver_test.clj
+++ doublets/test/doublets/solver_test.clj
@@ -19,3 +19,23 @@
   (testing "with no word links found"
     (is (= []
            (doublets "ye" "freezer")))))
+
+(deftest shared-letters-test
+  (testing "finds shared letters by position between two words"
+    (is (= [0 0 0 0] (shared-letters "abcd" "efgh")))
+    (is (= [1 0 0 0] (shared-letters "abcd" "aefg")))
+    (is (= [1 0 1 0] (shared-letters "abcd" "aecg")))
+    (is (= [1 1 1 1] (shared-letters "abcd" "abcd")))
+    (is (= [] (shared-letters "abcd" "abcde")))
+    (is (= [0 1 0 0] (shared-letters "abcd" "dbac")))))
+
+(deftest find-neighbors-test
+  (let [word-list ["zzzz" "abcd" "abce" "" "abc" "abcde" "bbcd" "abed"]]
+    (testing "finds neighbors in a search space"
+      (is (= #{"abce" "abed" "bbcd"} (into #{} (find-neighbors "abcd" word-list)))))))
+
+(deftest new-neighbors-test
+  (let [word-list ["zzzz" "abcd" "abce" "" "abc" "abcde" "bbcd" "abed"]
+        seen #{"abed"}]
+    (testing "returns only neighbors we haven't met yet"
+      (is (= #{"abce" "bbcd"} (into #{} (new-neighbors "abcd" word-list seen)))))))
